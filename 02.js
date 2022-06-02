cc3ea2a2b7e484682dace0727730aa667295c356
열정영웅 <103996469+houndhollis@users.noreply.github.com>
2022-06-02 21:48:17 +0900

    문자열 연산자

---
+++ 02.js
@@ -0,0 +1,32 @@
+const word = '문자'
+const word2 = "문자"
+const word3 = `문자`
+
+word[0] // 문
+word.length //2
+
+const words = word +' '+ word2 // 문자 문자
+ 
+// = 할당연산자 , === 비교연산자 + 산술연산자, &비트연산자, && 논리연산자,
+
+//let o1 = 'cat' || 'dog' ?  'cat'
+//let o2 = 'cat' && 'dog' ?  'dog
+
+const tempm  = 1 === '1' ? '참일때' : '거짓일때'; // '거짓일때'
+
+
+const data = 'a'
+const num = 0
+const boolean = true;
+
+const arr = [data,num,boolean,'name']
+arr.length;//4
+// 인덱스는 0부터 길이는 1부터 
+
+//메소드는 자바스크립에서 기본적으로 제공해주는 것이다.
+arr.push()
+arr.unshift()
+
+
+
+
